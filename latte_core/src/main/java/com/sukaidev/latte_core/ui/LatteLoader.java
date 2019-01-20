a3c93bb9eea6564472c7e8e07c55f7bf14c5172d
SHH <suhaihongvip@vip.qq.com>
2019-01-20 17:19:03 +0800

    Loading框架集成与完善1

---
+++ latte_core/src/main/java/com/sukaidev/latte_core/ui/LatteLoader.java
@@ -0,0 +1,16 @@
+package com.sukaidev.latte_core.ui;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDialog;
+
+/**
+ * Created by sukaidev on 2019/01/20.
+ */
+public class LatteLoader {
+
+    public static void showLoading(Context context,String type){
+
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+    }
+}

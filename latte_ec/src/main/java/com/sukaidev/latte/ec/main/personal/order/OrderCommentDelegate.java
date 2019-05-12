cc41796658c9017b8e908e45ece53eed26139837
sukaidev <suhaihong88@gmail.com>
2019-05-12 23:20:14 +0800

    自定义多图添加与删除控件设计与实现.

--- latte_ec/src/main/java/com/sukaidev/latte/ec/main/personal/order/OrderCommentDelegate.java
+++ latte_ec/src/main/java/com/sukaidev/latte/ec/main/personal/order/OrderCommentDelegate.java
@@ -1,5 +1,6 @@
 package com.sukaidev.latte.ec.main.personal.order;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -9,6 +10,10 @@ import androidx.annotation.Nullable;
 import com.sukaidev.latte.ec.R;
 import com.sukaidev.latte.ec.R2;
 import com.sukaidev.latte_core.delegates.LatteDelegate;
+import com.sukaidev.latte_core.ui.callback.CallbackManager;
+import com.sukaidev.latte_core.ui.callback.CallbackType;
+import com.sukaidev.latte_core.ui.callback.IGlobalCallback;
+import com.sukaidev.latte_core.widget.AutoPhotoLayout;
 import com.sukaidev.latte_core.widget.StarLayout;
 
 import butterknife.BindView;
@@ -23,6 +28,9 @@ public class OrderCommentDelegate extends LatteDelegate {
     @BindView(R2.id.custom_star_layout)
     StarLayout mStarLayout = null;
 
+    @BindView(R2.id.custom_auto_photo_layout)
+    AutoPhotoLayout mAutoPhotoLayout = null;
+
     @OnClick(R2.id.top_tv_comment_commit)
     void onClickSubmit() {
         Toast.makeText(getContext(), "评分："+mStarLayout.getStarCount(), Toast.LENGTH_LONG).show();
@@ -35,6 +43,13 @@ public class OrderCommentDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        mAutoPhotoLayout.setDelegate(this);
+        CallbackManager.getInstance()
+                .addCallback(CallbackType.ON_CROP, new IGlobalCallback<Uri>() {
+                    @Override
+                    public void executeCallback(@Nullable Uri args) {
+                        mAutoPhotoLayout.onCropTarget(args);
+                    }
+                });
     }
 }
